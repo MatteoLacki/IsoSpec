dc6e75b752b87864fd9699e11f9a359900fd32d6
michalsta <michal.startek.uw@gmail.com>
2017-01-10 19:17:25 +0100

    - Export the option to skip trimming excess configurations in C wrapper

--- IsoSpec++/cwrapper.h
+++ IsoSpec++/cwrapper.h
@@ -66,7 +66,8 @@ void* setupIsoLayered( int             _dimNumber,
                        int             tabSize,
                        int             hashSize,
                        double          step,
-                       bool            estimate
+                       bool            estimate,
+		       bool            trim
 );
 
 void* setupIsoOrdered( int             _dimNumber,
@@ -99,7 +100,8 @@ void* setupIso( int             _dimNumber,
                 int             algo,
                 int             tabSize,
                 int             hashSize,
-                double          step
+                double          step,
+		bool            trim
 );
 
 void* IsoFromFormula(const char* formula, double cutoff, int tabSize, int hashSize);
