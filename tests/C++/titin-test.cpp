1a03f7db7c24387ab936bcb9078f144472f45180
michalsta <michal.startek.uw@gmail.com>
2017-06-10 04:29:03 +0200

    - Some stuff

--- tests/C++/titin-test.cpp
+++ tests/C++/titin-test.cpp
@@ -10,51 +10,39 @@ int main()
     SSummator s;
     unsigned int cnt_tot = 0;
     int total_t = 10;
-    double threshold = 0.8;
-    double mmin = -1000000000000000000000000000000000000000000000000000000000000.0;
-    double mmax = 1000000000000000000000000000000000000000000000000000000000000.0;
+    double threshold = 0.1;
+//    double mmin = 3815900.0;
+//    double mmax = 3816000.0;
+    double mmin = -100000000000.0;
+    double mmax = 100000000000.0;
+    #if 1
+    IsoThresholdGenerator* iso = new IsoThresholdGenerator("C169719H270464N45688O52237S911", threshold, false);
+    unsigned int cnt = 0;
+    while(iso->advanceToNextConfiguration())
     {
-        IsoThresholdGenerator* iso = new IsoThresholdGenerator("C169719H270464N45688O52237S911", threshold, false);
-	std::cout << exp(iso->lprob()) << std::endl;
-	double last = 0.0;
-        unsigned int cnt = 0;
-        while(iso->advanceToNextConfiguration())
-        {
-            if(iso->mass() >= mmin and mmax >= iso->mass())
-                cnt++;
-	    s.add(exp(iso->lprob()));
-	    if(cnt % 10000000 == 0)
-		std::cout << cnt << "	" << s.get() << "\t" << exp(iso->lprob()) << '\n';
-	    last = exp(iso->lprob());
-        }
-	delete iso;
-	cnt_tot += cnt;
-	std::cout <<  "Slice: " << cnt << " element(s), last: " << last << std::endl;
+        if(iso->mass() >= mmin and mmax >= iso->mass())
+            cnt++;
+        s.add(exp(iso->lprob()));
     }
+    delete iso;
 
-    std::cout <<  "The isotopologue set containing at least 0.9 probability has " << cnt_tot << " element(s)" << std::endl;
-
+    std::cout <<  "The isotopologue set containing at least 0.9 probability has " << cnt << " element(s)" << std::endl;
+    std::cout << "prob: " << s.get() << std::endl;
+    #endif
+#if 0
     IsoThresholdGeneratorBoundMass* isob = new IsoThresholdGeneratorBoundMass("C169719H270464N45688O52237S911", threshold, mmin, mmax, false);
     std::cout << isob->getModeLProb() << std::endl;
 
     unsigned int confsig[5];
     double cnt = 1.0;
     cnt_tot = 0;
+    double lc = isob->getModeLProb() + log(threshold);
     while(isob->advanceToNextConfiguration())
     {
-    //    cnt *= 1.000000001;
     	cnt_tot++;
-//        s.add(exp(iso->lprob()));
-//        std::cout << "Lprob: " << iso->lprob() << std::endl;
-//        iso->get_conf_signature(confsig);
-//        printArray(confsig, 5);
     }
     delete isob;
 
     std::cout <<  "The isotopologue set containing at least 0.9 probability has " << cnt_tot << " element(s)" << std::endl;
-    std::cout << s.get() << cnt << std::endl;
-
-    //IsoThresholdGenerator* isosp = new IsoThresholdGenerator("C169719H270464N45688O52237S911", threshold, false);
-    //Spectrum sp(*isosp, 0.01);
-    //delete isosp;
+#endif
 }
