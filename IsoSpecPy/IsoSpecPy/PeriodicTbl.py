ec2689db5b38101ef59d8925754ee3e410a71eb4
michalsta <michal.startek.uw@gmail.com>
2017-08-15 01:54:07 +0200

    - Python3 fixes

--- IsoSpecPy/IsoSpecPy/PeriodicTbl.py
+++ IsoSpecPy/IsoSpecPy/PeriodicTbl.py
@@ -2,13 +2,19 @@ from IsoSpecPy import isoFFI
 from collections import defaultdict
 
 
+
+try: 
+    xrange
+except NameError:
+    xrange = range
+
 number_of_isotopic_entries = isoFFI.clib.NUMBER_OF_ISOTOPIC_ENTRIES
 
 symbol_to_masses = defaultdict(tuple)
 symbol_to_probs  = defaultdict(tuple)
 
 for i in xrange(number_of_isotopic_entries):
-    symbol = isoFFI.ffi.string(isoFFI.clib.elem_table_symbol[i])
+    symbol = isoFFI.ffi.string(isoFFI.clib.elem_table_symbol[i]).decode("ascii")
     symbol_to_masses[symbol] += (isoFFI.clib.elem_table_mass[i],)
     symbol_to_probs[symbol] += (isoFFI.clib.elem_table_probability[i],)
 
