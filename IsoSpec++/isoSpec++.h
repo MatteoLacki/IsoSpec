143c31b716eab1d815021d8bc87afaf9cf4484ec
michalsta <michal.startek.uw@gmail.com>
2017-09-29 18:01:59 +0200

    - Some whitespace fixes

--- IsoSpec++/isoSpec++.h
+++ IsoSpec++/isoSpec++.h
@@ -1,18 +1,18 @@
 /*
- *   Copyright (C) 2015-2016 Mateusz Łącki and Michał Startek.
- *
- *   This file is part of IsoSpec.
- *
- *   IsoSpec is free software: you can redistribute it and/or modify
- *   it under the terms of the Simplified ("2-clause") BSD licence.
- *
- *   IsoSpec is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- *   You should have received a copy of the Simplified BSD Licence
- *   along with IsoSpec.  If not, see <https://opensource.org/licenses/BSD-2-Clause>.
- */
+*   Copyright (C) 2015-2016 Mateusz Łącki and Michał Startek.
+*
+*   This file is part of IsoSpec.
+*
+*   IsoSpec is free software: you can redistribute it and/or modify
+*   it under the terms of the Simplified ("2-clause") BSD licence.
+*
+*   IsoSpec is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+*   You should have received a copy of the Simplified BSD Licence
+*   along with IsoSpec.  If not, see <https://opensource.org/licenses/BSD-2-Clause>.
+*/
 
 
 #ifndef ISOSPEC_PLUS_PLUS_HPP
@@ -30,8 +30,8 @@
 
 
 #ifdef BUILDING_R
- #include <Rcpp.h>
- using namespace Rcpp;
+#include <Rcpp.h>
+using namespace Rcpp;
 #endif /* BUILDING_R */
 
 
@@ -42,35 +42,35 @@ class IsoThresholdGenerator;
 
 class Iso {
 private:
-	void setupMarginals(const double** _isotopeMasses, const double** _isotopeProbabilities);
+    void setupMarginals(const double** _isotopeMasses, const double** _isotopeProbabilities);
 public:
         bool disowned;
 protected:
-	int 			dimNumber;
-	int*			isotopeNumbers;
-	int*			atomCounts;
-	unsigned int	confSize;
-	int			    allDim;
-	Marginal**              marginals;
+    int 			dimNumber;
+    int*			isotopeNumbers;
+    int*			atomCounts;
+    unsigned int	confSize;
+    int			    allDim;
+    Marginal**              marginals;
         double                  modeLProb;
 
 public:
-	Iso(
-	    int             _dimNumber,
-	    const int*      _isotopeNumbers,
-	    const int*      _atomCounts,
-	    const double**  _isotopeMasses,
-	    const double**  _isotopeProbabilities
-	);
+    Iso(
+        int             _dimNumber,
+        const int*      _isotopeNumbers,
+        const int*      _atomCounts,
+        const double**  _isotopeMasses,
+        const double**  _isotopeProbabilities
+    );
 
-	Iso(const char* formula);
+    Iso(const char* formula);
     Iso(Iso&& other);
     Iso(const Iso& other, bool fullcopy);
 
-	virtual ~Iso();
+    virtual ~Iso();
 
-	double getLightestPeakMass() const;
-	double getHeaviestPeakMass() const;
+    double getLightestPeakMass() const;
+    double getHeaviestPeakMass() const;
     inline double getModeLProb() const { return modeLProb; };
     inline int getDimNumber() const { return dimNumber; };
     inline int getAllDim() const { return allDim; };
@@ -86,20 +86,20 @@ public:
 class IsoGenerator : public Iso
 {
 protected:
-        double* partialLProbs;
-        double* partialMasses;
-        double* partialExpProbs;
+    double* partialLProbs;
+    double* partialMasses;
+    double* partialExpProbs;
 
 public:
-	virtual bool advanceToNextConfiguration() = 0;
-        inline const double& lprob() const { return partialLProbs[0]; };
-        inline const double& mass()  const { return partialMasses[0]; };
-        inline const double& eprob() const { return partialExpProbs[0]; };
+    virtual bool advanceToNextConfiguration() = 0;
+    inline const double& lprob() const { return partialLProbs[0]; };
+    inline const double& mass()  const { return partialMasses[0]; };
+    inline const double& eprob() const { return partialExpProbs[0]; };
 //	virtual const int* const & conf() const = 0;
 
 
     inline IsoGenerator(Iso&& iso);
-	inline virtual ~IsoGenerator() { delete[] partialLProbs; delete[] partialMasses; delete[] partialExpProbs; };
+    inline virtual ~IsoGenerator() { delete[] partialLProbs; delete[] partialMasses; delete[] partialExpProbs; };
 
 };
 
@@ -157,23 +157,23 @@ public:
     };
 
     IsoThresholdGenerator(Iso&& iso, double _threshold, bool _absolute=true,
-                          int _tabSize=1000, int _hashSize=1000);
+                        int _tabSize=1000, int _hashSize=1000);
 
     inline virtual ~IsoThresholdGenerator() { delete[] counter;
-                                              delete[] maxConfsLPSum;
-                                              dealloc_table(marginalResults, dimNumber); };
+                                            delete[] maxConfsLPSum;
+                                            dealloc_table(marginalResults, dimNumber); };
 
     void terminate_search();
 
 private:
     inline void recalc(int idx)
     {
-	for(; idx >=0; idx--)
-	{
-	    partialLProbs[idx] = partialLProbs[idx+1] + marginalResults[idx]->get_lProb(counter[idx]);
-	    partialMasses[idx] = partialMasses[idx+1] + marginalResults[idx]->get_mass(counter[idx]);
+        for(; idx >=0; idx--)
+        {
+            partialLProbs[idx] = partialLProbs[idx+1] + marginalResults[idx]->get_lProb(counter[idx]);
+            partialMasses[idx] = partialMasses[idx+1] + marginalResults[idx]->get_mass(counter[idx]);
             partialExpProbs[idx] = partialExpProbs[idx+1] * marginalResults[idx]->get_eProb(counter[idx]);
-	}
+        }
     }
 
 
@@ -184,29 +184,29 @@ private:
 class IsoThresholdGeneratorBoundMass : public IsoGenerator
 {
 private:
-	double* maxConfsLPSum, *minMassCSum, *maxMassCSum;
-	double Lcutoff;
-        double min_mass, max_mass;
-        RGTMarginal** marginalResults;
+    double* maxConfsLPSum, *minMassCSum, *maxMassCSum;
+    double Lcutoff;
+    double min_mass, max_mass;
+    RGTMarginal** marginalResults;
 
 public:
     //TODO work on virtuals
-	virtual bool advanceToNextConfiguration();
+    virtual bool advanceToNextConfiguration();
 //        virtual inline void get_conf_signature(unsigned int* target);
 //	virtual const int* const & conf() const;
 
-        IsoThresholdGeneratorBoundMass(Iso&& iso, double  _threshold, double min_mass, double max_mass, bool _absolute = true, int _tabSize  = 1000, int _hashSize = 1000);
+    IsoThresholdGeneratorBoundMass(Iso&& iso, double  _threshold, double min_mass, double max_mass, bool _absolute = true, int _tabSize  = 1000, int _hashSize = 1000);
 
-	virtual ~IsoThresholdGeneratorBoundMass();
+    virtual ~IsoThresholdGeneratorBoundMass();
 
 private:
-	void setup_ith_marginal_range(unsigned int idx);
-        inline void recalc(int idx)
-        {
-            partialLProbs[idx] = partialLProbs[idx+1] + marginalResults[idx]->current_lProb();
-            partialMasses[idx] = partialMasses[idx+1] + marginalResults[idx]->current_mass();
-            partialExpProbs[idx] = partialExpProbs[idx+1] * marginalResults[idx]->current_eProb();
-        }
+    void setup_ith_marginal_range(unsigned int idx);
+    inline void recalc(int idx)
+    {
+        partialLProbs[idx] = partialLProbs[idx+1] + marginalResults[idx]->current_lProb();
+        partialMasses[idx] = partialMasses[idx+1] + marginalResults[idx]->current_mass();
+        partialExpProbs[idx] = partialExpProbs[idx+1] * marginalResults[idx]->current_eProb();
+    }
 
 
 
@@ -218,32 +218,32 @@ private:
 class IsoThresholdGeneratorMT : public IsoGenerator
 {
 private:
-	unsigned int* counter;
-	double* maxConfsLPSum;
-	const double Lcutoff;
-        SyncMarginal* last_marginal;
-        PrecalculatedMarginal** marginalResults;
+    unsigned int* counter;
+    double* maxConfsLPSum;
+    const double Lcutoff;
+    SyncMarginal* last_marginal;
+    PrecalculatedMarginal** marginalResults;
 
 public:
-	virtual bool advanceToNextConfiguration();
-        virtual inline void get_conf_signature(unsigned int* target) { memcpy(target, counter, sizeof(unsigned int)*dimNumber); };
+    virtual bool advanceToNextConfiguration();
+    virtual inline void get_conf_signature(unsigned int* target) { memcpy(target, counter, sizeof(unsigned int)*dimNumber); };
 //	virtual const int* const & conf() const;
 
-        IsoThresholdGeneratorMT(Iso&& iso, double  _threshold, PrecalculatedMarginal** marginals, bool _absolute = true);
+    IsoThresholdGeneratorMT(Iso&& iso, double  _threshold, PrecalculatedMarginal** marginals, bool _absolute = true);
 
-	inline virtual ~IsoThresholdGeneratorMT() { delete[] counter; delete[] maxConfsLPSum;};
-        void terminate_search();
+    inline virtual ~IsoThresholdGeneratorMT() { delete[] counter; delete[] maxConfsLPSum;};
+    void terminate_search();
 
 private:
-	inline void recalc(int idx)
-	{
-		for(; idx >=0; idx--)
-		{
-			partialLProbs[idx] = partialLProbs[idx+1] + marginalResults[idx]->get_lProb(counter[idx]);
-			partialMasses[idx] = partialMasses[idx+1] + marginalResults[idx]->get_mass(counter[idx]);
-                        partialExpProbs[idx] = partialExpProbs[idx+1] * marginalResults[idx]->get_eProb(counter[idx]);
-		}
-	}
+    inline void recalc(int idx)
+    {
+        for(; idx >=0; idx--)
+        {
+            partialLProbs[idx] = partialLProbs[idx+1] + marginalResults[idx]->get_lProb(counter[idx]);
+            partialMasses[idx] = partialMasses[idx+1] + marginalResults[idx]->get_mass(counter[idx]);
+            partialExpProbs[idx] = partialExpProbs[idx+1] * marginalResults[idx]->get_eProb(counter[idx]);
+        }
+    }
 
 
 
@@ -311,10 +311,10 @@ private:
 
 #ifndef BUILDING_R
 
- void printConfigurations(
-     const   std::tuple<double*,double*,int*,int>& results,
-     int     dimNumber,
-     int*    isotopeNumbers
- );
+void printConfigurations(
+    const   std::tuple<double*,double*,int*,int>& results,
+    int     dimNumber,
+    int*    isotopeNumbers
+);
 #endif
 #endif /* ISOSPEC_PLUS_PLUS_HPP */
