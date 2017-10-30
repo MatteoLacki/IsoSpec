af0e55c163d76495e94ed134b7a20b052305ca10
michalsta <michal.startek.uw@gmail.com>
2017-10-30 13:35:47 +0100

    - Several conv. dicts in PeriodicTbl

--- IsoSpecPy/IsoSpecPy/PeriodicTbl.py
+++ IsoSpecPy/IsoSpecPy/PeriodicTbl.py
@@ -18,3 +18,17 @@ for i in xrange(number_of_isotopic_entries):
 
 symbol_to_masses = dict(symbol_to_masses)
 symbol_to_probs = dict(symbol_to_probs)
+
+# Several derivative convenience dicts...
+symbol_to_massprob = dict((key, [zip(symbol_to_masses[key], symbol_to_probs[key])]) for key in symbol_to_probs.keys())
+
+def crossprod(l1, l2):
+    return sum(x1*x2 for x1, x2 in zip(l1, l2))
+
+symbol_to_avg_mass = dict((key, crossprod(symbol_to_masses[key], symbol_to_probs[key])) for key in symbol_to_probs.keys())
+
+def maxprod(l1, l2):
+    return max(zip(l1, l2), key = lambda x: x[1])[0]
+
+symbol_to_monoisotopic_mass = dict((key, maxprod(symbol_to_masses[key], symbol_to_probs[key])) for key in symbol_to_probs.keys())
+
