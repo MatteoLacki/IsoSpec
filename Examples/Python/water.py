cb0a41f399f3458b01c63d2bac36f1bd117d6ba4
grzebrze4 <grzebrze4@gmail.com>
2018-10-04 23:02:24 +0200

    - Make Python example compatible with Python3 (while keeping Python2 compat.)

--- Examples/Python/water.py
+++ Examples/Python/water.py
@@ -7,26 +7,26 @@ try:
     if IsoSpecPy.__version__[:3] != '1.9':
         raise AttributeError
 except AttributeError:
-    print "This file is meant to be used with IsoSpecPy version 1.9.X. You seem to have a different version installed on your system."
+    print("This file is meant to be used with IsoSpecPy version 1.9.X. You seem to have a different version installed on your system.")
     import sys
     sys.exit(-1)
 
 i = IsoSpecPy.IsoOrderedGenerator(formula="H2O1", get_confs=True)
 
-print "Calculating isotopic distribution of water. Here's a list of all configurations, in a guaranteed order of nonincreasing probability:"
+print("Calculating isotopic distribution of water. Here's a list of all configurations, in a guaranteed order of nonincreasing probability:")
 
 for mass, log_prob, conf in i:
-    print "Mass:", mass
-    print "log(probability):", log_prob
-    print "probability:", exp(log_prob)
-    print "Number of Protium atoms:", conf[0][0]
-    print "Number of Deuterium atoms", conf[0][1]
-    print "Number of O16 atoms:", conf[1][0]
-    print "Number of O17 atoms:", conf[1][1]
-    print "Number of O18 atoms:", conf[1][2]
+    print("Mass: " + str(mass))
+    print("log(probability): " + str(log_prob))
+    print("probability: " + str(exp(log_prob)))
+    print("Number of Protium atoms: " + str(conf[0][0]))
+    print("Number of Deuterium atoms: " + str(conf[0][1]))
+    print("Number of O16 atoms: " + str(conf[1][0]))
+    print("Number of O17 atoms: " + str(conf[1][1]))
+    print("Number of O18 atoms: " + str(conf[1][2]))
 
-print
-print "Now what if both isotopes of hydrogen were equally probable, while prob. of O16 was 50%, O17 at 30% and O18 at 20%?"
+print("")
+print("Now what if both isotopes of hydrogen were equally probable, while prob. of O16 was 50%, O17 at 30% and O18 at 20%?")
 
 hydrogen_probs = (0.5, 0.5)
 oxygen_probs = (0.5, 0.3, 0.2)
@@ -36,17 +36,18 @@ oxygen_masses = (15.99491461956, 16.99913170, 17.9991610)
 i = IsoSpecPy.IsoOrderedGenerator(atomCounts = (2, 1), isotopeMasses = (hydrogen_masses, oxygen_masses), isotopeProbabilities = (hydrogen_probs, oxygen_probs), get_confs=True)
 
 
-mass, log_prob, conf = i.__iter__().next()
+mass, log_prob, conf = next(i.__iter__())
 
-print "The first configuration has the following parameters:"
-print "Mass:", mass
-print "log(probability):", log_prob
-print "probability:", exp(log_prob)
-print "Number of Protium atoms:", conf[0][0]
-print "Number of Deuterium atoms", conf[0][1]
-print "Number of O16 atoms:", conf[1][0]
-print "Number of O17 atoms:", conf[1][1]
-print "Number of O18 atoms:", conf[1][2]
+print("The first configuration has the following parameters:")
+print("Mass: " + str(mass))
+print("log(probability): " + str(log_prob))
+print("probability: " + str(exp(log_prob)))
+print("Number of Protium atoms: " + str(conf[0][0]))
+print("Number of Deuterium atoms: " + str(conf[0][1]))
+print("Number of O16 atoms: " + str(conf[1][0]))
+print("Number of O17 atoms: " + str(conf[1][1]))
+print("Number of O18 atoms: " + str(conf[1][2]))
+
 
 
 
