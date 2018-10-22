f75769ebdba10ec115c3aed27bde2b3b7141ed5b
Michał Startek <mist@mimuw.edu.pl>
2018-10-22 19:12:10 +0200

    Make from_formula a little bit less ad-hoc

--- tests/C++/from_formula.cpp
+++ tests/C++/from_formula.cpp
@@ -16,20 +16,21 @@ int main(int argc, char** argv)
 	IsoOrderedGenerator i(argv[1]);
 	double target_prob = atof(argv[2]);
 	int no_visited = 0;
+        int* space = new int[i.getAllDim()];
 	while(target_prob > 0.0 && i.advanceToNextConfiguration())
 	{
 		target_prob -= i.eprob();
 		no_visited += 1;
 #if PRINT_CONFS
                 std::cout << "EPROB: " << i.eprob() << "  \tMASS: " << i.mass() << "\tCONF: ";
-                int space[5];
                 i.get_conf_signature(space);
-                for(int i=0; i<5; i++)
-                    std::cout << space[i] << " ";
+                for(int ii=0; ii<i.getAllDim(); ii++)
+                    std::cout << space[ii] << " ";
                 std::cout << std::endl;
 
 #endif /* PRINT_CONFS */
 	}
+        delete[] space;
 	std::cout << "The number of visited configurations is:" << no_visited << std::endl;
 
 }
