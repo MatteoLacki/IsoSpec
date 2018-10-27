d4123da8ad25bd4c18ddbfe0490c78fed03288c6
michalsta <michal.startek.uw@gmail.com>
2018-10-27 14:48:10 +0200

    Expose test function in from_formula_threshold.cpp

--- tests/C++/from_formula_threshold.cpp
+++ tests/C++/from_formula_threshold.cpp
@@ -4,6 +4,10 @@
 
 using namespace IsoSpec;
 
+#ifndef ISOSPEC_TESTS_SKIP_MAIN
+
+size_t test_threshold(const char* formula, double threshold, bool print_confs);
+
 int main(int argc, char** argv)
 {
 	if(argc < 3)
@@ -12,13 +16,24 @@ int main(int argc, char** argv)
 		std::cout << "...will the configurations with probability above 0.01 for the above molecule" << std::endl;
 		return -1;
 	}
-	double target_prob = atof(argv[2]);
-	IsoThresholdGenerator i(argv[1], target_prob, true, 100, 100, true);
+	size_t no_visited = test_threshold(argv[1], atof(argv[2]), true);
+	
+	std::cout << "The number of visited configurations is:" << no_visited << std::endl;
+
+}
+#endif /* ISOSPEC_TESTS_SKIP_MAIN */
+
+
+size_t test_threshold(const char* formula, double threshold, bool print_confs)
+{
+
+	IsoThresholdGenerator i(formula, threshold, true, 100, 100, true);
         size_t confs_no = i.count_confs();
-        std::cout << "No. confs is: " << confs_no << std::endl;
+	if(print_confs)
+        	std::cout << "No. confs is: " << confs_no << std::endl;
         i.reset();
-        IsoThresholdGenerator i2(argv[1], target_prob, true, 100, 100, true);
-        IsoThresholdGenerator i3(argv[1], target_prob, true, 100, 100, false);
+        IsoThresholdGenerator i2(formula, threshold, true, 100, 100, true);
+        IsoThresholdGenerator i3(formula, threshold, true, 100, 100, false);
         int* confspace = new int[i.getAllDim()];
         int* confspace2 = new int[i2.getAllDim()];
         //int* confspace3 = new int[i3.getAllDim()]; // these will be in different order...
@@ -28,14 +43,16 @@ int main(int argc, char** argv)
 	{
                 assert(i2.advanceToNextConfiguration());
                 assert(i3.advanceToNextConfiguration());
-                std::cout << "lprob: " << i.lprob() << " prob: " << i.eprob() << " log(prob): " << log(i.eprob()) << " mass: " << i.mass() << " conf: ";
+		if(print_confs)
+                	std::cout << "lprob: " << i.lprob() << " prob: " << i.eprob() << " log(prob): " << log(i.eprob()) << " mass: " << i.mass() << " conf: ";
                 assert(i.lprob() == i2.lprob());
                 assert(i.mass() == i2.mass());
                 assert(i.eprob() == i2.eprob());
                 i.get_conf_signature(confspace);
                 i2.get_conf_signature(confspace2);
                 assert(memcmp(confspace, confspace2, i.getAllDim()*sizeof(int)) == 0);
-                printArray<int>(confspace, i.getAllDim());
+		if(print_confs)
+                	printArray<int>(confspace, i.getAllDim());
 		no_visited += 1;
                 total_prob += i.eprob();
 	}
@@ -43,6 +60,6 @@ int main(int argc, char** argv)
         delete[] confspace2;
         assert(!i2.advanceToNextConfiguration());
         assert(!i3.advanceToNextConfiguration());
-	std::cout << "The number of visited configurations is:" << no_visited << " for total prob of: " << total_prob << std::endl;
+	return no_visited;
 
 }
