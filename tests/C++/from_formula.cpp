36de6a7df87417286211b4d8ecc886536bda74b8
michalsta <michal.startek.uw@gmail.com>
2018-10-27 14:22:14 +0200

    Improve Ordered test, make it export a test function

--- tests/C++/from_formula.cpp
+++ tests/C++/from_formula.cpp
@@ -3,34 +3,53 @@
 
 using namespace IsoSpec;
 
-#define PRINT_CONFS true
+#ifndef ISOSPEC_TESTS_SKIP_MAIN
+size_t test_ordered(const char* formula, double total_prob, bool print_confs = false);
 
 int main(int argc, char** argv)
 {
 	if(argc < 3)
 	{
-		std::cout << "Proper usage (for example): ./from_formula C10000H1000O1000N1000 0.9999" << std::endl;
+		std::cout << "Proper usage (for example): " << argv[0] << " C10000H1000O1000N1000 0.9999" << std::endl;
 		std::cout << "...will print the minimal number of configurations necessary to cover 0.9999 probability of the above molecule" << std::endl;
 		return -1;
 	}
-	IsoOrderedGenerator i(argv[1]);
-	double target_prob = atof(argv[2]);
-	int no_visited = 0;
+	
+	#ifndef ISOSPEC_TESTS_MEMSAN
+	size_t no_confs = 
+	#endif 
+			  test_ordered(argv[1], atof(argv[2]), true);
+
+	#ifndef ISOSPEC_TESTS_MEMSAN
+	std::cout << "The number of visited configurations is: " << no_confs << std::endl;
+	#endif
+}
+
+#endif /* ISOSPEC_TESTS_SKIP_MAIN */
+
+
+size_t test_ordered(const char* formula, double total_prob, bool print_confs)
+{
+	IsoOrderedGenerator i(formula);
+	double target_prob = total_prob;
+
+	size_t no_visited = 0;
         int* space = new int[i.getAllDim()];
 	while(target_prob > 0.0 && i.advanceToNextConfiguration())
 	{
 		target_prob -= i.eprob();
 		no_visited += 1;
-#if PRINT_CONFS
-                std::cout << "EPROB: " << i.eprob() << "  \tMASS: " << i.mass() << "\tCONF: ";
-                i.get_conf_signature(space);
-                for(int ii=0; ii<i.getAllDim(); ii++)
-                    std::cout << space[ii] << " ";
-                std::cout << std::endl;
+		if(print_confs)
+		{
+			std::cout << "EPROB: " << i.eprob() << "  \tMASS: " << i.mass() << "\tCONF: ";
+			i.get_conf_signature(space);
+			for(int ii=0; ii<i.getAllDim(); ii++)
+			    std::cout << space[ii] << " ";
+			std::cout << std::endl;
+		}
 
-#endif /* PRINT_CONFS */
 	}
         delete[] space;
-	std::cout << "The number of visited configurations is:" << no_visited << std::endl;
+	return no_visited;
 
 }
