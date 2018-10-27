ed8367900b2e08c14851107965efc403a85717c5
michalsta <michal.startek.uw@gmail.com>
2018-10-27 14:35:50 +0200

    Export test function in from_formula_layered.cpp

--- tests/C++/from_formula_layered.cpp
+++ tests/C++/from_formula_layered.cpp
@@ -3,8 +3,9 @@
 
 using namespace IsoSpec;
 
-#define PRINT_CONFS true
+size_t test_layered(const char* formula, double total_prob, bool print_confs = false);
 
+#ifndef ISOSPEC_TESTS_SKIP_MAIN
 int main(int argc, char** argv)
 {
 	if(argc < 3)
@@ -13,23 +14,32 @@ int main(int argc, char** argv)
 		std::cout << "...will print the minimal number of configurations necessary to cover 0.9999 probability of the above molecule" << std::endl;
 		return -1;
 	}
-	double target_prob = atof(argv[2]);
-	IsoLayeredGenerator i(argv[1], target_prob, 0.3, 1000, 1000, true);
-	int no_visited = 0;
+
+	size_t no_confs = test_layered(argv[1], atof(argv[2]), true);
+
+	std::cout << "The number of visited configurations is:" << no_confs << std::endl;
+}
+#endif /* #ifndef ISOSPEC_TESTS_SKIP_MAIN */
+
+size_t test_layered(const char* formula, double total_prob, bool print_confs)
+{
+	IsoLayeredGenerator i(formula, total_prob, 0.3, 1000, 1000, true);
+	size_t no_visited = 0;
         int* space = new int[i.getAllDim()];
 	while(i.advanceToNextConfiguration())
 	{
 		no_visited += 1;
-#if PRINT_CONFS
-                std::cout << "EPROB: " << i.eprob() << "  \tMASS: " << i.mass() << "\tCONF: ";
-                i.get_conf_signature(space);
-                for(int ii=0; ii<i.getAllDim(); ii++)
-                    std::cout << space[ii] << " ";
-                std::cout << std::endl;
+		if(print_confs)
+		{
+			std::cout << "EPROB: " << i.eprob() << "  \tMASS: " << i.mass() << "\tCONF: ";
+			i.get_conf_signature(space);
+			for(int ii=0; ii<i.getAllDim(); ii++)
+			    std::cout << space[ii] << " ";
+			std::cout << std::endl;
+		}
 
-#endif /* PRINT_CONFS */
 	}
         delete[] space;
-	std::cout << "The number of visited configurations is:" << no_visited << std::endl;
 
+	return no_visited;
 }
