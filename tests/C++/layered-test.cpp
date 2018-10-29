deb2016e2d2b4d634eac54ea8f5f15ee5218e820
michalsta <michal.startek.uw@gmail.com>
2018-10-29 23:37:43 +0100

    Fix layered test a bit

--- tests/C++/layered-test.cpp
+++ tests/C++/layered-test.cpp
@@ -9,16 +9,16 @@ int main()
 {
 
     SSummator s;
-    unsigned int cnt_tot = 0;
-    int total_t = 10;
-    double threshold = -300.0;
+//    unsigned int cnt_tot = 0;
+//    int total_t = 10;
+    double threshold = 0.999;
 //    double mmin = 3815900.0;
 //    double mmax = 3816000.0;
     double mmin = -100000000000.0;
     double mmax = 100000000000.0;
-    #if 1
-//    IsoLayeredGenerator* iso = new IsoLayeredGenerator("C1600H2700N1000", -100.0);
-    IsoLayeredGenerator* iso = new IsoLayeredGenerator("H2C2", 0.5, 0.3);
+
+    IsoLayeredGenerator* iso = new IsoLayeredGenerator("C1600H2700N1000", threshold, 0.3);
+//    IsoLayeredGenerator* iso = new IsoLayeredGenerator("H2C2", 0.5, 0.3);
     unsigned int cnt = 0;
     int cntr[10];
     while(iso->advanceToNextConfiguration())
@@ -35,23 +35,6 @@ int main()
     }
     delete iso;
 
-    std::cout <<  "The isotopologue set containing at least 0.9 probability has " << cnt << " element(s)" << std::endl;
+    std::cout <<  "The isotopologue set containing at least " << threshold << " probability has " << cnt << " element(s)" << std::endl;
     std::cout << "prob: " << s.get() << std::endl;
-    #endif
-#if 0
-    IsoThresholdGeneratorBoundMass* isob = new IsoThresholdGeneratorBoundMass("C169719H270464N45688O52237S911", threshold, mmin, mmax, false);
-    std::cout << isob->getModeLProb() << std::endl;
-
-    unsigned int confsig[5];
-    double cnt = 1.0;
-    cnt_tot = 0;
-    double lc = isob->getModeLProb() + log(threshold);
-    while(isob->advanceToNextConfiguration())
-    {
-    	cnt_tot++;
-    }
-    delete isob;
-
-    std::cout <<  "The isotopologue set containing at least 0.9 probability has " << cnt_tot << " element(s)" << std::endl;
-#endif
 }
