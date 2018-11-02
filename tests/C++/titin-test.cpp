5b52097d1d010173a62bf365e0d593287a098771
Michał Startek <mist@mimuw.edu.pl>
2018-11-02 23:42:51 +0100

    Some messy improvements

--- tests/C++/titin-test.cpp
+++ tests/C++/titin-test.cpp
@@ -10,39 +10,25 @@ int main()
     SSummator s;
 //    unsigned int cnt_tot = 0;
 //    int total_t = 10;
-    double threshold = 0.1;
+    double threshold = 0.01;
 //    double mmin = 3815900.0;
 //    double mmax = 3816000.0;
     double mmin = -100000000000.0;
     double mmax = 100000000000.0;
-    #if 1
-    IsoThresholdGenerator* iso = new IsoThresholdGenerator("C169719H270464N45688O52237S911", threshold, false);
+    const char formula[] = "C169719H270464N45688O52237S911";
+//    const char formula[] = "H2O1";
+//    const char formula[] = "C63H98N18O13S1"; // substance P
+//    const char formula[] = "C520H817N139O147S8"; // Human insulin
+    IsoThresholdGenerator* iso = new IsoThresholdGenerator(formula, threshold, false);
     unsigned int cnt = 0;
     while(iso->advanceToNextConfiguration())
     {
         if(iso->mass() >= mmin and mmax >= iso->mass())
             cnt++;
-        s.add(exp(iso->lprob()));
+        s.add(iso->prob());
     }
     delete iso;
 
     std::cout <<  "The isotopologue set containing at least 0.9 probability has " << cnt << " element(s)" << std::endl;
     std::cout << "prob: " << s.get() << std::endl;
-    #endif
-#if 0
-    IsoThresholdGeneratorBoundMass* isob = new IsoThresholdGeneratorBoundMass("C169719H270464N45688O52237S911", threshold, mmin, mmax, false);
-    std::cout << isob->getModeLProb() << std::endl;
-
-    unsigned int confsig[5];
-    double cnt = 1.0;
-    cnt_tot = 0;
-    double lc = isob->getModeLProb() + log(threshold);
-    while(isob->advanceToNextConfiguration())
-    {
-    	cnt_tot++;
-    }
-    delete isob;
-
-    std::cout <<  "The isotopologue set containing at least 0.9 probability has " << cnt_tot << " element(s)" << std::endl;
-#endif
 }
