a9c23438ec98403e9f93666f25941d11c059bd88
Michał Startek <mist@mimuw.edu.pl>
2018-11-09 16:13:11 +0100

    Remove tabs

--- IsoSpecR/src/Rinterface.cpp
+++ IsoSpecR/src/Rinterface.cpp
@@ -26,94 +26,94 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 NumericMatrix Rinterface(
-	const IntegerVector& 	molecule,
-	const DataFrame& 		isotopes,
-	double 	stopCondition,
-	int		algo 		= 0,
-	int 	tabSize 	= 1000,
-	int		hashSize 	= 1000,
-	double 	step 		= .25,
-	bool 	showCounts  = false,
-	bool	trim 		= true
+    const IntegerVector&    molecule,
+    const DataFrame&        isotopes,
+    double                  stopCondition,
+    int                     algo = 0,
+    int                     tabSize = 1000,
+    int                     hashSize = 1000,
+    double                  step = .3,
+    bool                    showCounts = false,
+    bool                    trim = true
 ){
 
-	unsigned int dimNumber = molecule.size();
-	std::vector<int> 	stdIsotopeNumbers;
-	std::vector<double> stdIsotopeMasses;
-	std::vector<double> stdIsotopeProbabilities;
+    unsigned int dimNumber = molecule.size();
+    std::vector<int>     stdIsotopeNumbers;
+    std::vector<double> stdIsotopeMasses;
+    std::vector<double> stdIsotopeProbabilities;
 
-	const CharacterVector& element = isotopes["element"];
-	const CharacterVector& isotope = isotopes["isotope"];
-	const NumericVector& mass 	   = isotopes["mass"];
-	const NumericVector& abundance = isotopes["abundance"];
-	const CharacterVector& molecule_names = molecule.names();
+    const CharacterVector& element = isotopes["element"];
+    const CharacterVector& isotope = isotopes["isotope"];
+    const NumericVector& mass        = isotopes["mass"];
+    const NumericVector& abundance = isotopes["abundance"];
+    const CharacterVector& molecule_names = molecule.names();
 
-	CharacterVector stdIsotopeTags = CharacterVector::create("mass", "logProb");
+    CharacterVector stdIsotopeTags = CharacterVector::create("mass", "logProb");
 
-	for (unsigned int i=0; i<dimNumber; i++)
-	{
-		unsigned int counter = 0;
-		for (int j=0; j<element.size(); j++)
-			if( element[j] == molecule_names[i] )
-			{
-				counter++;
-				stdIsotopeMasses.push_back( mass[j] );
-				stdIsotopeProbabilities.push_back( abundance[j] );
-				if( showCounts )
-					stdIsotopeTags.push_back( isotope[j] );
-			}
-		stdIsotopeNumbers.push_back(counter);
-	}
+    for (unsigned int i=0; i<dimNumber; i++)
+    {
+        unsigned int counter = 0;
+        for (int j=0; j<element.size(); j++)
+            if( element[j] == molecule_names[i] )
+            {
+                counter++;
+                stdIsotopeMasses.push_back( mass[j] );
+                stdIsotopeProbabilities.push_back( abundance[j] );
+                if( showCounts )
+                    stdIsotopeTags.push_back( isotope[j] );
+            }
+        stdIsotopeNumbers.push_back(counter);
+    }
 
-	IsoSpec* iso = reinterpret_cast<IsoSpec*>(setupIso(
-		dimNumber,
-		stdIsotopeNumbers.data(),
-		Rcpp::as<std::vector<int> >( molecule).data(),
-		stdIsotopeMasses.data(),
-		stdIsotopeProbabilities.data(),
-		stopCondition,
-		algo,
-		tabSize,
-		hashSize,
-		step,
+    IsoSpec* iso = reinterpret_cast<IsoSpec*>(setupIso(
+        dimNumber,
+        stdIsotopeNumbers.data(),
+        Rcpp::as<std::vector<int> >( molecule).data(),
+        stdIsotopeMasses.data(),
+        stdIsotopeProbabilities.data(),
+        stopCondition,
+        algo,
+        tabSize,
+        hashSize,
+        step,
         trim
-	));
+    ));
 
-	int confsNo = iso->getNoVisitedConfs();
-	int columnsNo = stdIsotopeTags.size(); // standard
+    int confsNo = iso->getNoVisitedConfs();
+    int columnsNo = stdIsotopeTags.size(); // standard
 
-	int isotopesNo = 0;
-	for( std::vector<int>::iterator it=stdIsotopeNumbers.begin(); it != stdIsotopeNumbers.end(); it++ ){
-		isotopesNo += *it;
-	}
+    int isotopesNo = 0;
+    for( std::vector<int>::iterator it=stdIsotopeNumbers.begin(); it != stdIsotopeNumbers.end(); it++ ){
+        isotopesNo += *it;
+    }
 
-	NumericMatrix res(confsNo, columnsNo);
+    NumericMatrix res(confsNo, columnsNo);
 
-	int i = 0;
-	int j = 0;
+    int i = 0;
+    int j = 0;
 
-	for( std::vector<void*>::iterator it = iso->newaccepted.begin(); it != iso->newaccepted.end(); it++)
-	{
-		int* curr_conf  = getConf(*it);
-		res(i,0) = combinedSum( curr_conf, iso->masses, dimNumber );
-		res(i,1) = getLProb(*it);
+    for( std::vector<void*>::iterator it = iso->newaccepted.begin(); it != iso->newaccepted.end(); it++)
+    {
+        int* curr_conf  = getConf(*it);
+        res(i,0) = combinedSum( curr_conf, iso->masses, dimNumber );
+        res(i,1) = getLProb(*it);
 
-		j = 0;
-		for(unsigned int isotopeNumber=0; isotopeNumber < dimNumber; isotopeNumber++)
-		{
-			int currentConfIndex = curr_conf[isotopeNumber];
-			int locIsoNo = stdIsotopeNumbers[isotopeNumber];
+        j = 0;
+        for(unsigned int isotopeNumber=0; isotopeNumber < dimNumber; isotopeNumber++)
+        {
+            int currentConfIndex = curr_conf[isotopeNumber];
+            int locIsoNo = stdIsotopeNumbers[isotopeNumber];
 
-			if( showCounts )
-				for( int k=0; k<locIsoNo; k++)
-					res(i,2+j+k) = (*(iso->marginalConfs[isotopeNumber]))[currentConfIndex][k];
-			j += locIsoNo;
-		}
-		i++;
-	}
+            if( showCounts )
+                for( int k=0; k<locIsoNo; k++)
+                    res(i,2+j+k) = (*(iso->marginalConfs[isotopeNumber]))[currentConfIndex][k];
+            j += locIsoNo;
+        }
+        i++;
+    }
 
-	colnames(res) = stdIsotopeTags; //This is RCPP sugar. It sucks.
-	destroyIso(iso);
+    colnames(res) = stdIsotopeTags; //This is RCPP sugar. It sucks.
+    destroyIso(iso);
 
-	return(res);
+    return(res);
 }
