e201df6babdf7a66dd952261a416680eff769f97
matteo <matteo.lacki@gmail.com>
2018-11-17 23:54:27 +0100

    Added 3 tests for molecules.

--- IsoSpecR/R/IsoSpecR.R
+++ IsoSpecR/R/IsoSpecR.R
@@ -39,13 +39,11 @@ NULL
 #' the percentage of the highest peak below which isotopologues get pruned.
 #' @param isotopes  A named list of isotopic information required for IsoSpec. The names must be valid element symbols, see \code{isotopicData} for examples. Each enlisted object should be a \code{data.frame} containing columns \code{element} (specifying the symbol of the element), \code{mass} (specifying the mass of the isotope), \code{abundance} (specyfying the assumed frequency of finding that isotope).
 #' @param step      The percent of the the percentile of isotopologues in the current isolayer, specyfying the cutoff for the next isolayer. It has been optimised and better not change the default value.
-#' @param tabSize   A technical parameter: the initial size of the \code{C++} dynamic table containing the results. Better not change the default value.
 #' @return A numeric matrix containing the masses, the logarithms of probability, and, optionally, counts of isotopologues. Attention: this matrix does not have to be sorted. Sorting it would also compromise the linear complexity of our algorithm.
 #' @examples
 #' res <- IsoSpecify( molecule = c(C=10,H=22,O=1), stopCondition = .9999 )
 #' print(res)
 #' @export
-
 IsoSpecify <- function(
         molecule,
         stopCondition,
@@ -53,8 +51,7 @@ IsoSpecify <- function(
         showCounts = FALSE,
         trim    = TRUE,
         algo    = 0,
-        step    = .25,
-        tabSize = 1000
+        step    = .25
 ){
     if(is.null(isotopes)){
         isotopes <- isotopicData$IsoSpec
@@ -65,7 +62,7 @@ IsoSpecify <- function(
         isotopes        = isotopes,
         stopCondition   = stopCondition,
         algo            = as.integer(algo),
-        tabSize         = tabSize,
+        tabSize         = 1000,
         hashSize        = 1000,
         step            = step,
         showCounts      = showCounts,
