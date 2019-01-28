d89915bb9d9ea8f8640c8a6a8a1ef41b3560882c
Michał Startek <mist@mimuw.edu.pl>
2019-01-28 11:24:24 +0100

    Fix R radiolabelling example

--- Examples/R/radiolabelling.R
+++ Examples/R/radiolabelling.R
@@ -1,26 +1,32 @@
 library(IsoSpecR)
 
-### DOES NOT WORK YET
 
 # We will model a glucose molecule where one of the carbons has been replaced with 
 # (14C) with 95% probability.
 
 
 # First, we define an alternate isotopic table for elements that contains the readiocarbon: 14C with 95% probability, 
-# and either 12C or 13C with their standard relative probabilities otherwise. First, we load up standard isotopic data:
+# and either 12C or 13C with their natural relative probabilities otherwise. First, we load up standard isotopic data:
 data(isotopicData)
 isotopes = isotopicData$IsoSpec
 
-# And we add an artificial element X, with 3 isotopes that will represent the radiocarbon. 
-# This part is a bit messy...
-levels(isotopes[[2]]) = c(levels(isotopes[[2]]), "X12", "X13", "X14")
-isotopes = rbind(isotopes, c("X", "X12", 12.0, 0.05*0.989211941850467, 0))
-isotopes = rbind(isotopes, c("X", "X13", 13.0033548352, 0.05*0.0107880581495331, 0))
-isotopes = rbind(isotopes, c("X", "X14", 14.003241989, 0.95, 0))
+# And we add an artificial element X, with 3 isotopes, that will represent the radiocarbon. 
+radiolabel = data.frame(
+    element = c('X', 'X', 'X'),
+    isotope = c('12X', '13X', '14X'),
+    # Grab the masses of 12C and 13C from IsoSpec builtin data, provide mass of 14C manually
+    mass = c(isotopes[isotopes$isotope == '12C', 'mass'], isotopes[isotopes$isotope == '13C', 'mass'], 14.003241989),
+    # Isotope abundances are: 95% 14C, and the remaining 5% is split between 12C and 13C proportionally to their natural abundances
+    abundance = c(isotopes[isotopes$isotope == '12C', 'abundance'] * 0.05, isotopes[isotopes$isotope == '13C', 'abundance'] * 0.05, 0.95),
+    # unused parameter, kept for backward compatibility, ignore that
+    ratioC = c(NA, NA, NA)
+)
+
+isotopes = rbind(isotopes, radiolabel)
 
 
 
-radioglucose = c(C=5, H=12, O=6, X=1) # A glucose molecule: one of the carbons is replaced with an artificial element X
+radioglucose = c(C=5, H=12, O=6, X=1) # A glucose molecule: one of the carbons is replaced with an artificial element X (the readiocarbon)
 
 p = .99 # joint probability of the output
 
