b2e2092dcaacf7c70f2578e54079a116cb6c4282
Michał Startek <mist@mimuw.edu.pl>
2019-03-22 19:03:26 +0100

    Reimplement tabulators to new API

--- IsoSpec++/tabulator.cpp
+++ IsoSpec++/tabulator.cpp
@@ -3,6 +3,143 @@
 
 namespace IsoSpec
 {
-template class Tabulator<IsoThresholdGenerator>;
-template class Tabulator<IsoLayeredGenerator>;
+
+Tabulator::Tabulator() : 
+_masses(nullptr),
+_lprobs(nullptr),
+_probs(nullptr),
+_confs(nullptr),
+_confs_no(0)
+{}
+
+Tabulator::~Tabulator()
+{
+    if( _masses != nullptr ) free(_masses);
+    if( _lprobs != nullptr ) free(_lprobs);
+    if( _probs  != nullptr ) free(_probs);
+    if( _confs  != nullptr ) free(_confs);
+}
+
+
+ThresholdTabulator::ThresholdTabulator(IsoThresholdGenerator* generator, 
+                                       bool get_masses, bool get_probs,
+                                       bool get_lprobs, bool get_confs) : 
+Tabulator()
+{
+    _confs_no = generator->count_confs();
+    const int allDim = generator->getAllDim();
+
+    if(get_masses) _masses = (double *) malloc(_confs_no * sizeof(double));
+    if(get_lprobs) _lprobs = (double *) malloc(_confs_no * sizeof(double));
+    if(get_probs)  _probs  = (double *) malloc(_confs_no * sizeof(double));
+    if(get_confs)  _confs  = (int *)    malloc(_confs_no * allDim*sizeof(int));
+
+    while(generator->advanceToNextConfiguration())
+    {
+        if(_masses != nullptr) { *_masses = generator->mass();  _masses++; }
+        if(_lprobs != nullptr) { *_lprobs = generator->lprob(); _lprobs++; }
+        if(_probs  != nullptr) { *_probs  = generator->prob();  _probs++;  }
+        if(_confs  != nullptr){
+            generator->get_conf_signature(_confs);
+            _confs += allDim;
+        }
+    }
+
+    if(_masses != nullptr) { _masses -= _confs_no; }
+    if(_lprobs != nullptr) { _lprobs -= _confs_no; }
+    if(_probs  != nullptr) { _probs -= _confs_no; }
+    if(_confs  != nullptr) { _confs -= _confs_no*allDim; }
+
+}
+
+ThresholdTabulator::~ThresholdTabulator() {}
+
+
+LayeredTabulator::LayeredTabulator(IsoLayeredGenerator* ILG,
+                     bool get_masses, bool get_probs,
+                     bool get_lprobs, bool get_confs,
+                     double _target_total_prob, bool _optimize) : 
+Tabulator(),
+generator(ILG),
+target_total_prob(_target_total_prob),
+current_size(ISOSPEC_INIT_TABLE_SIZE),
+optimize(_optimize),
+allDim(generator->getAllDim())
+{
+    if(_target_total_prob <= 0.0)
+        return;
+
+    bool user_wants_probs = get_probs;
+    if(optimize)
+    // If we want to optimize, we need the probs
+        get_probs = true;
+
+    if(get_masses) _masses = (double *) malloc(ISOSPEC_INIT_TABLE_SIZE * sizeof(double));
+    if(get_lprobs) _lprobs = (double *) malloc(ISOSPEC_INIT_TABLE_SIZE * sizeof(double));
+    if(get_probs)  _probs  = (double *) malloc(ISOSPEC_INIT_TABLE_SIZE * sizeof(double));
+    if(get_confs)  _confs  = (int *)    malloc(ISOSPEC_INIT_TABLE_SIZE * generator->getAllDim() * sizeof(int));
+
+    tmasses = _masses;
+    tlprobs = _lprobs;
+    tprobs = _probs;
+    tconfs = _confs;
+
+    size_t last_switch = 0;
+    double prob_at_last_switch = 0.0;
+    double prob_so_far = 0.0;
+
+    do 
+    { // Store confs until we accumulatoe more prob than needed - and, if optimizing, 
+      // store also the rest of the layer
+        while(generator->advanceToNextConfigurationWithinLayer())
+        {
+            addConf();
+            prob_so_far += generator->prob();
+            if(!optimize && prob_so_far >= target_total_prob)
+                return;
+        }
+        if(prob_so_far >= target_total_prob)
+            break;
+        last_switch = _confs_no;
+        prob_at_last_switch = prob_so_far;
+    } while(generator->nextLayer(-3.0));
+
+    if(!optimize)
+        return;
+
+    throw std::logic_error("hahaha, lol, nope, not yet implemented");
+
+    if(!user_wants_probs)
+    {
+        free(_probs);
+        _probs = nullptr;
+    }
+}
+
+
+
+void LayeredTabulator::addConf()
+{
+    if( _confs_no == current_size )
+    {
+        current_size *= 2;
+
+        // FIXME: Handle overflow gracefully here. It definitely could happen for people still stuck on 32 bits...
+
+        if(_masses != nullptr) { _masses = (double*) realloc(_masses, current_size * sizeof(double)); tmasses = _masses + _confs_no; }
+        if(_lprobs != nullptr) { _lprobs = (double*) realloc(_lprobs, current_size * sizeof(double)); tlprobs = _lprobs + _confs_no; }
+        if(_probs  != nullptr) { _probs  = (double*) realloc(_probs, current_size * sizeof(double));  tprobs  = _probs  + _confs_no; }
+        if( _confs != nullptr) { _confs  = (int*)    realloc(_confs, current_size * allDim * sizeof(int)); tconfs = _confs + (allDim * _confs_no); }
+    }
+
+    if(_masses != nullptr) { *tmasses = generator->mass();  tmasses++; };
+    if(_lprobs != nullptr) { *tlprobs = generator->lprob(); tlprobs++; };
+    if(_probs  != nullptr) { *tprobs  = generator->prob();  tprobs++;  };
+    if(_confs  != nullptr) { generator->get_conf_signature(tconfs); tconfs += allDim; };
+
+    _confs_no++;
+}
+
+LayeredTabulator::~LayeredTabulator() {}
+
 } // namespace IsoSpec
--- IsoSpec++/tabulator.h
+++ IsoSpec++/tabulator.h
@@ -9,26 +9,25 @@
 namespace IsoSpec
 {
 
-template <typename T> class Tabulator
+
+class Tabulator
 {
-private:
+protected:
     double* _masses;
     double* _lprobs;
     double* _probs;
     int*    _confs;
     size_t  _confs_no;
 public:
-    Tabulator(T* generator,
-              bool get_masses, bool get_probs,
-              bool get_lprobs, bool get_confs);
-
-    ~Tabulator();
+    Tabulator();
+    virtual ~Tabulator();
 
     inline double*   masses(bool release = false)   { double* ret = _masses; if(release) _masses = nullptr; return ret; };
     inline double*   lprobs(bool release = false)   { double* ret = _lprobs; if(release) _lprobs = nullptr; return ret; };
     inline double*   probs(bool release = false)    { double* ret = _probs;  if(release) _probs  = nullptr; return ret; };
     inline int*      confs(bool release = false)    { int*    ret = _confs;  if(release) _confs  = nullptr; return ret; };
     inline size_t    confs_no() { return _confs_no; };
+
 };
 
 inline void reallocate(double **array, int new_size){
@@ -37,9 +36,52 @@ inline void reallocate(double **array, int new_size){
     }
 }
 
+
+class ThresholdTabulator : public Tabulator
+{
+public:
+    ThresholdTabulator(IsoThresholdGenerator* ITG, 
+                       bool get_masses, bool get_probs,
+                       bool get_lprobs, bool get_confs);
+
+    virtual ~ThresholdTabulator();
+};
+
+
+
+class LayeredTabulator : public Tabulator
+{
+public:
+    LayeredTabulator(IsoLayeredGenerator* ILG,
+                     bool get_masses, bool get_probs,
+                     bool get_lprobs, bool get_confs,
+                     double _total_prob, bool _optimize = false);
+
+    virtual ~LayeredTabulator();
+
+private:
+    IsoLayeredGenerator* generator;
+    double target_total_prob;
+    size_t current_size;
+    bool optimize;
+    const int allDim;
+    void addConf();
+
+    double* tmasses;
+    double* tprobs;
+    double* tlprobs;
+    int* tconfs;
+};
+
+
+
+
+/*
+
 template <typename T> Tabulator<T>::Tabulator(T* generator,
                      bool get_masses, bool get_probs,
-                     bool get_lprobs, bool get_confs  )
+                     bool get_lprobs, bool get_confs,
+                     bool , double )
 {
     size_t current_size = ISOSPEC_INIT_TABLE_SIZE;
     int confs_tbl_idx = 0;
@@ -52,50 +94,44 @@ template <typename T> Tabulator<T>::Tabulator(T* generator,
     _probs  = get_probs  ? (double *) malloc(ISOSPEC_INIT_TABLE_SIZE * sizeof(double)) : nullptr;
     _confs  = get_confs  ? (int *)    malloc(ISOSPEC_INIT_TABLE_SIZE * allDimSizeOfInt): nullptr;
 
+    do{
+        while(generator->advanceToNextConfiguration()){
+            if( _confs_no == current_size )
+            {
+                current_size *= 2;
 
-    while(generator->advanceToNextConfiguration()){
-        if( _confs_no == current_size )
-        {
-            current_size *= 2;
+                // FIXME: Handle overflow gracefully here. It definitely could happen for people still stuck on 32 bits...
 
-            // FIXME: Handle overflow gracefully here. It definitely could happen for people still stuck on 32 bits...
+                reallocate(&_masses, current_size * sizeof(double));
+                reallocate(&_lprobs, current_size * sizeof(double));
+                reallocate(&_probs,  current_size * sizeof(double));
 
-            reallocate(&_masses, current_size * sizeof(double));
-            reallocate(&_lprobs, current_size * sizeof(double));
-            reallocate(&_probs,  current_size * sizeof(double));
-
-            if( _confs != nullptr ){
-                _confs = (int *) realloc(_confs, current_size * allDimSizeOfInt);
+                if( _confs != nullptr ){
+                    _confs = (int *) realloc(_confs, current_size * allDimSizeOfInt);
+                }
             }
+
+            if(_masses != nullptr) _masses[_confs_no] = generator->mass();
+
+            if(_lprobs != nullptr) _lprobs[_confs_no] = generator->lprob();
+
+            if(_probs  != nullptr) _probs[_confs_no]  = generator->prob();
+
+            if(_confs  != nullptr){
+                generator->get_conf_signature(_confs + confs_tbl_idx);
+                confs_tbl_idx += generator->getAllDim();
+            }
+
+            _confs_no++;
         }
-
-        if(_masses != nullptr) _masses[_confs_no] = generator->mass();
-
-        if(_lprobs != nullptr) _lprobs[_confs_no] = generator->lprob();
-
-        if(_probs  != nullptr) _probs[_confs_no]  = generator->prob();
-
-        if(_confs  != nullptr){
-            generator->get_conf_signature(_confs + confs_tbl_idx);
-            confs_tbl_idx += generator->getAllDim();
-        }
-
-        _confs_no++;
-    }
+    } while(generator->nextLayer(-3.0));
 
     _masses = (double *) realloc(_masses, _confs_no * sizeof(double));
     _lprobs = (double *) realloc(_lprobs, _confs_no * sizeof(double));
     _probs  = (double *) realloc(_probs,  _confs_no * sizeof(double));
     _confs  = (int *)    realloc(_confs,  confs_tbl_idx * sizeof(int));
 }
-
-template <typename T> Tabulator<T>::~Tabulator()
-{
-    if( _masses != nullptr ) free(_masses);
-    if( _lprobs != nullptr ) free(_lprobs);
-    if( _probs  != nullptr ) free(_probs);
-    if( _confs  != nullptr ) free(_confs);
-}
+*/
 
 } // namespace IsoSpec
 
