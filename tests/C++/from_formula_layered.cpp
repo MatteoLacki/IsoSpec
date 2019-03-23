630b49d78f14a5aa67941fb94dbdb9fbbef3c6fe
Michał Startek <mist@mimuw.edu.pl>
2019-03-23 16:27:56 +0100

    Make from_formula more suitable for performance testing

--- tests/C++/from_formula_layered.cpp
+++ tests/C++/from_formula_layered.cpp
@@ -12,11 +12,19 @@ int main(int argc, char** argv)
 	if(argc < 3)
 	{
 		std::cout << "Proper usage (for example): ./from_formula_layered C10000H1000O1000N1000 0.9999" << std::endl;
-		std::cout << "...will print the minimal number of configurations necessary to cover 0.9999 probability of the above molecule" << std::endl;
+		std::cout << "...will print the configurations necessary to cover 0.9999 probability of the above molecule" << std::endl;
+                std::cout << "./from_formula_layered C10000H1000O1000N1000 0.9999 false" << std::endl;
+                std::cout << "will just count them" << std::endl;
+
 		return -1;
 	}
 
-	size_t no_confs = test_layered_tabulator(argv[1], atof(argv[2]), true);
+        bool print_confs = true;
+
+        if(argc > 3)
+            print_confs = (strcmp(argv[3], "true") == 0);
+
+	size_t no_confs = test_layered_tabulator(argv[1], atof(argv[2]), print_confs);
 
 	std::cout << "The number of visited configurations is:" << no_confs << std::endl;
 }
