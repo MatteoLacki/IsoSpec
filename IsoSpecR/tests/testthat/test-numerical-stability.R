aa617eb668508433bdb2489ddebbe7472ec264c8
matteo <matteo.lacki@gmail.com>
2019-04-24 08:35:23 +0200

    Test turned off for a while.

--- IsoSpecR/tests/testthat/test-numerical-stability.R
+++ IsoSpecR/tests/testthat/test-numerical-stability.R
@@ -3,7 +3,7 @@
 # test_that("IsoSpecR calculates correctly full isotopic distributions.", {
 #   expect_equal(nrow(IsoSpecify(c(C=100, H=202), 1)), 101*203)
 #   expect_equal(nrow(IsoSpecify(c(C=100, H=202, S=5), 1)), 101*203*choose(4+5-1, 5))
-#   expect_equal(nrow(IsoSpecify(c(O=100, N=10, S=6), 1)), choose(100+3-1,100)*11*choose(4+6-1, 6))
+#   expect_equal(nrow(IsoSpecify(c(O=100, N=10, S=6), 1)), choose(100+3-1,100)*choose(10+2-1, 10)*choose(4+6-1, 6))
 # })
 # 
 # test_that("IsoSpecR provides results similar to envipat.", {
