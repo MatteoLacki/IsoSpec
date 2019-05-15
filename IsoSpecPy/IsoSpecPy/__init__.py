4b614c605fad1e2f22e401b123e71c6791e653b0
Michał Startek <mist@mimuw.edu.pl>
2019-05-15 17:20:48 +0200

    Filename change

--- IsoSpecPy/IsoSpecPy/__init__.py
+++ IsoSpecPy/IsoSpecPy/__init__.py
@@ -4,7 +4,7 @@ from .IsoSpecPy import *
 # For backward compatibility with 1.0.X:
 class CompatIsoWrapper(object):
     def __init__(self):
-        from .IsoSpecPy import IsoSpec, IsoSpecify, IsoPlot
+        from .IsoSpecPyOld import IsoSpec, IsoSpecify, IsoPlot
         self.IsoSpec = IsoSpec
         self.IsoSpecify = IsoSpecify
         self.IsoPlot = IsoPlot
