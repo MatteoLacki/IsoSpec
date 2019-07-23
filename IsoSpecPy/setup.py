ea7d2ba6c09747219cffa6b2ad071b5a16e48b57
michalsta <michal.startek.uw@gmail.com>
2019-07-23 21:10:23 +0200

    Mark version 2.0.2b1

--- IsoSpecPy/setup.py
+++ IsoSpecPy/setup.py
@@ -41,7 +41,7 @@ setup_args = {
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    'version': '2.0.1',
+    'version': '2.0.2b1',
 
     'description': 'Python interface to IsoSpec++ isotopic envelope calculator library',
     'long_description': 'Python interface to IsoSpec++ isotopic envelope calculator library',
