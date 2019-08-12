b7b37896e6f59a4bdfcc6e1e2e92646a2cdc8f46
Michał Startek <mist@mimuw.edu.pl>
2019-08-12 21:55:12 +0200

    Py3 compat

--- tests/Python/test_iface.py
+++ tests/Python/test_iface.py
@@ -4,5 +4,5 @@ from IsoSpecPy.Formulas import *
 glu = IsoSpecPy.IsoTotalProb(0.99, formula=glucose)
 ca = IsoSpecPy.IsoTotalProb(0.99, formula=caffeine)
 
-print ca.wassersteinDistance(glu)
+print(ca.wassersteinDistance(glu))
 
