9a2613b30087969cc76ae3a798f51a2dccdf1cd0
Michał Startek <mist@mimuw.edu.pl>
2020-01-02 19:52:18 +0100

    Implement IsoStochasticGenerator

--- IsoSpec++/isoSpec++.h
+++ IsoSpec++/isoSpec++.h
@@ -504,12 +504,89 @@ class IsoStochastic
     double beta_bias;
     double confs_prob;
     double chasing_prob;
+    size_t current_count;
 
 public:
     IsoStochastic(Iso&& iso, size_t no_molecules, double precision = 0.9999, double beta_bias = 5.0);
 
-    ISOSPEC_FORCE_INLINE bool next()
+    ISOSPEC_FORCE_INLINE size_t count() const { return current_count; };
+
+    ISOSPEC_FORCE_INLINE double mass() const { return ILG.mass(); };
+
+    ISOSPEC_FORCE_INLINE double prob() const { return ILG.prob(); };
+
+    ISOSPEC_FORCE_INLINE double lprob() const { return ILG.lprob(); };
+
+    ISOSPEC_FORCE_INLINE void get_conf_signature(int* space) const { ILG.get_conf_signature(space); };
+
+    ISOSPEC_FORCE_INLINE bool advanceToNextConfiguration()
     {
+        while(true)
+        {
+            double curr_conf_prob_left, current_prob;
+
+            if(to_sample_left <= 0)
+                return false;
+
+            if(confs_prob < chasing_prob)
+            {
+                // Beta was last
+                current_count = 1;
+                to_sample_left--;
+                ILG.advanceToNextConfiguration();
+                current_prob = ILG.prob();
+                confs_prob += current_prob;
+                while(confs_prob <= chasing_prob)
+                {
+                    ILG.advanceToNextConfiguration();
+                    current_prob = ILG.prob();
+                    confs_prob += current_prob;
+                }
+                if(to_sample_left <= 0)
+                    return true;
+                curr_conf_prob_left = confs_prob - chasing_prob;
+            }
+            else
+            {
+                // Binomial was last
+                current_count = 0;
+                ILG.advanceToNextConfiguration();
+                current_prob = ILG.prob();
+                confs_prob += current_prob;
+                curr_conf_prob_left = current_prob;
+            }
+
+            double prob_left_to_1 = precision - chasing_prob;
+            double expected_confs = curr_conf_prob_left * to_sample_left / prob_left_to_1;
+
+            if(expected_confs <= beta_bias)
+            {
+                // Beta mode: we keep making beta jumps until we leave the current configuration
+                chasing_prob += rdvariate_beta_1_b(to_sample_left) * prob_left_to_1;
+                while(chasing_prob <= confs_prob)
+                {
+                    current_count++;
+                    to_sample_left--;
+                    if(to_sample_left == 0)
+                        return true;
+                    prob_left_to_1 = precision - chasing_prob;
+                    chasing_prob += rdvariate_beta_1_b(to_sample_left) * prob_left_to_1;
+                }
+                if(current_count > 0)
+                    return true;
+            }
+            else
+            {
+                // Binomial mode: a single binomial step
+                size_t rbin = rdvariate_binom(to_sample_left, curr_conf_prob_left/prob_left_to_1);
+                current_count += rbin;
+                to_sample_left -= rbin;
+                chasing_prob = confs_prob;
+                if(current_count > 0)
+                    return true;
+            }
+        };
+
     }
 };
 
