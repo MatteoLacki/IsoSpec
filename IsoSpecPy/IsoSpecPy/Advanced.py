8b0dad560276939a62098932b11269632ab0f85d
Michał Startek <mist@mimuw.edu.pl>
2020-01-16 09:21:18 +0100

    Some convenience functions for developers

---
+++ IsoSpecPy/IsoSpecPy/Advanced.py
@@ -0,0 +1,26 @@
+'''This file contains a set of functions useful for the analysis of properties of
+isotopic envelopes. These are unlikely to be of any interest to end-users, only
+for developers interested in testing some properties of the isotopic envelopes.
+These are implemented mostly in Python, and so, not as fast as the rest of the
+package.'''
+
+def _int_neighbours(subisotopologue_conf):
+    conf = list(subisotopologue_conf)
+    for i in range(len(conf)):
+        for j in range(len(conf)):
+            if i != j and conf[i] > 0:
+                conf[i] -= 1
+                conf[j] += 1
+                yield tuple(conf)
+                conf[i] += 1
+                conf[j] -= 1
+
+
+def neighbours(conf):
+    for i in range(len(conf)):
+        for si_neigh in _int_neighbours(conf[i]):
+            yield tuple(conf[j] if j != i else si_neigh for j in range(len(conf)))
+
+
+print(list(neighbours(((10,0,0),(5,5)))))
+                
