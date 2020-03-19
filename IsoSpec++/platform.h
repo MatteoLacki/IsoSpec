7380d39ddcf9061ef8b6bef6cb5612a5f1259023
Michał Startek <mist@mimuw.edu.pl>
2020-03-19 19:58:52 +0100

    cpplint fixes

--- IsoSpec++/platform.h
+++ IsoSpec++/platform.h
@@ -51,11 +51,11 @@
 #endif
 
 #if !defined(ISOSPEC_USE_PTHREADS)
-#define ISOSPEC_USE_PTHREADS false /* TODO: possibly put a macro here to detect whether we */
-#endif                             /* can/should use pthreads - or rip them out altogether.
-                                    * Investigate whether the performance advantage of pthreads on
-                                    * some platforms (*cough* CYGWIN *cough*) is still large
-                                    * enough to justify keeping both implementations around */
+#define ISOSPEC_USE_PTHREADS false // TODO(who knows?): possibly put a macro here to detect whether we
+#endif                             // can/should use pthreads - or rip them out altogether.
+                                   // Investigate whether the performance advantage of pthreads on
+                                   // some platforms (*cough* CYGWIN *cough*) is still large
+                                   // enough to justify keeping both implementations around
 
 #if !defined(ISOSPEC_WE_ARE_ON_UNIX_YAY)
 #define ISOSPEC_WE_ARE_ON_UNIX_YAY ISOSPEC_TEST_WE_ARE_ON_UNIX_YAY
