487640fff7711fc74cb489f1d815ee53fc8ae75d
Michał Startek <mist@mimuw.edu.pl>
2020-06-10 20:03:36 +0200

    Make printing of confs optional

--- tests/C++/from_formula_ordered.cpp
+++ tests/C++/from_formula_ordered.cpp
@@ -12,13 +12,19 @@ int main(int argc, char** argv)
 	{
 		std::cout << "Proper usage (for example): " << argv[0] << " C10000H1000O1000N1000 0.9999" << std::endl;
 		std::cout << "...will print the minimal number of configurations necessary to cover 0.9999 probability of the above molecule" << std::endl;
+                std::cout << argv[0] << " C10000H1000O1000N1000 0.9999 false" << std::endl;
+                std::cout << "will just count them" << std::endl;
 		return -1;
 	}
+        bool print_confs = true;
+
+        if(argc > 3)
+            print_confs = (strcmp(argv[3], "true") == 0);
 	
 	#ifndef ISOSPEC_TESTS_MEMSAN
 	size_t no_confs = 
 	#endif 
-			  test_ordered(argv[1], atof(argv[2]), true);
+			  test_ordered(argv[1], atof(argv[2]), print_confs);
 
 	#ifndef ISOSPEC_TESTS_MEMSAN
 	std::cout << "The number of visited configurations is: " << no_confs << std::endl;
