689db43ec9898c62db3255b466ddc6c38b67cd4e
Filippo Rusconi <lopippo@debian.org>
2020-06-10 13:57:21 +0200

    Rework the CMake-based build system.
    The aim of the modifications is:
    
    - to establish a framework in which it is easy to add support for new platforms;
    - to create a build system that sets the basis for the Debian packaging;
    - to add the creation of a static version of the library which is then
      integrated in the Debian package;
    - add some goodies like a pkg-config file and CMake-based
      find_package-oriented files;
    - ensure that it is possible to select the install directory.

---
+++ CMakeStuff/modules/FindIsoSpec++.cmake
@@ -0,0 +1,34 @@
+# Copyright : Filippo Rusconi
+# License : GPL-3.0+
+# Authors : Filippo Rusconi
+
+find_path(IsoSpec++_INCLUDE_DIRS IsoSpec++/isoSpec++.h
+	PATHS /usr/local/include /usr/include
+	PATH_SUFFIXES isospec++ libisospec++ ENV PATH)
+
+find_library(IsoSpec++_LIBRARY NAMES isospec libisospec isospec++ libisospec++)
+
+if(IsoSpec++_INCLUDE_DIRS AND IsoSpec++_LIBRARY)
+
+	set(IsoSpec++_FOUND TRUE)
+
+endif()
+
+if(IsoSpec++_FOUND)
+
+	if (NOT IsoSpec++_FIND_QUIETLY)
+		message(STATUS "Found IsoSpec++_LIBRARY: ${IsoSpec++_LIBRARY}")
+	endif()
+
+	if(NOT TARGET IsoSpec++::IsoSpec++)
+
+		add_library(IsoSpec++::IsoSpec++ UNKNOWN IMPORTED)
+
+		set_target_properties(IsoSpec++::IsoSpec++ PROPERTIES
+			IMPORTED_LOCATION             "${IsoSpec++_LIBRARY}"
+			INTERFACE_INCLUDE_DIRECTORIES "${IsoSpec++_INCLUDE_DIRS}")
+
+	endif()
+
+endif()
+
