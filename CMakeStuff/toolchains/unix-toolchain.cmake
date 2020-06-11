1f4c20df3424bdbea8e4ec7cab18e163c6490b7a
Filippo Rusconi <lopippo@debian.org>
2020-06-11 09:02:27 +0200

    Change CMake install modules dir to cmake/isospec++ (++ were missing)
    And many other minor fixes/improvements/additions:
    
    Implementing the handling of the documentation.
    
    Change the install directory of the doc because the doc
    if for the whole package and not only for the C++ lib.
    
    Fix the bad substitutions in the pkg-config file, so as to preserve
    strings like ${prefix}. Use the @ONLY argument to the configure_file
    function call.

--- CMakeStuff/toolchains/unix-toolchain.cmake
+++ CMakeStuff/toolchains/unix-toolchain.cmake
@@ -12,7 +12,7 @@ set(CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES /usr/include)
 ## platform dependent compiler flags:
 include(CheckCXXCompilerFlag)
 
-if (WITH_FPIC)
+if(WITH_FPIC)
 	add_definitions(-fPIC)
 endif()
 
@@ -21,52 +21,22 @@ install(FILES ${CMAKE_MODULE_PATH}/FindIsoSpec++.cmake
 	DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/isospec++)
 
 # Install cmake config
-configure_file (${CMAKE_MODULE_PATH}/IsoSpec++Config.cmake.in
+configure_file(${CMAKE_MODULE_PATH}/IsoSpec++Config.cmake.in
 	${CMAKE_BINARY_DIR}/IsoSpec++Config.cmake)
 install(FILES ${CMAKE_BINARY_DIR}/IsoSpec++Config.cmake 
 	DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/isospec++)
 
-# Install the PkgConfig config file
-configure_file (${CMAKE_MODULE_PATH}/pkgconfig/libisospec++.pc.in
-	${CMAKE_BINARY_DIR}/libisospec++.pc)
+# Install the PkgConfig config file (only substitute the @VAR@
+# because we need to preserve all the ${prefix} strings like
+# they are.
+configure_file(${CMAKE_MODULE_PATH}/pkgconfig/libisospec++.pc.in
+	${CMAKE_BINARY_DIR}/libisospec++.pc @ONLY)
 install(FILES ${CMAKE_BINARY_DIR}/libisospec++.pc 
 	DESTINATION ${CMAKE_INSTALL_LIBDIR}/pkgconfig)
 
-# Prepare the logo picture files with the right version (configure_file)
+# Now deal with the manual doc stuff
+add_subdirectory(man)
 
-configure_file(${CMAKE_SOURCE_DIR}/CMakeStuff/isospec_logo2_high.svg.in
-	${CMAKE_SOURCE_DIR}/man/images/isospec_logo2_high_versioned.svg @ONLY)
-
-configure_file(${CMAKE_SOURCE_DIR}/CMakeStuff/isospec_logo2_long.svg.in
-	${CMAKE_SOURCE_DIR}/man/images/isospec_logo2_long_versioned.svg @ONLY)
-
-# Make the conversion of the svg file into a png, but only on GNU/Linux
-# Produce a file with respected aspect ratio, 200 pixels wide.
-
-if(UNIX AND NOT APPLE)
-	execute_process(COMMAND gm convert -geometry 200x
-		${CMAKE_SOURCE_DIR}/man/images/isospec_logo2_high_versioned.svg
-		${CMAKE_SOURCE_DIR}/man/images/isospec_logo2_high_versioned.png)
-endif()
-
-
-if(UNIX AND NOT APPLE)
-	execute_process(COMMAND gm convert -geometry 200x
-		${CMAKE_SOURCE_DIR}/man/images/isospec_logo2_long_versioned.svg
-		${CMAKE_SOURCE_DIR}/man/images/isospec_logo2_long_versioned.png)
-endif()
-
-# Ensure that the doxyfile configuration file for Doxygen has always
-# the proper version number!
-
-configure_file(${CMAKE_SOURCE_DIR}/CMakeStuff/doxyfile.in
-	${CMAKE_SOURCE_DIR}/man/doxyfile @ONLY)
-
-# Command:
-# make doc
-add_custom_target(doc
-	COMMAND doxygen man/doxyfile 
-	WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-	COMMENT "Doxygen-based developer documentation generation")
-
+# Now deal with the example  stuff
+add_subdirectory(Examples)
 
