fec0679eb1ea47a424c8d54b22726cfc5e89424c
Filippo Rusconi <lopippo@debian.org>
2020-06-22 22:01:17 +0200

    Ensure the DLL export/import magic is applied even when outside OpenMS

--- CMakeStuff/toolchains/win10-mingw64-toolchain.cmake
+++ CMakeStuff/toolchains/win10-mingw64-toolchain.cmake
@@ -8,6 +8,10 @@ if(WIN32 OR _WIN32)
 endif()
 
 
+message(STATUS "${BoldGreen}Setting definition -DISOSPEC_MAKE_DLL for symbol DLL export.${ColourReset}")
+add_definitions(-DISOSPEC_MAKE_DLL)
+
+
 # On Win10 all the code is relocatable.
 remove_definitions(-fPIC -Wall -pedantic -Wextra)
 
