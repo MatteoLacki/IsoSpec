05c8db812881ab601008feb36a9119bcef4e15d6
Michał Startek <mist@mimuw.edu.pl>
2020-06-22 17:35:52 +0200

    Extensive warnings

--- IsoSpec++/platform.h
+++ IsoSpec++/platform.h
@@ -93,7 +93,7 @@
 #define ISOSPEC_FORCE_INLINE inline
 #endif
 
-#if ISOSPEC_DEBUG
+#ifndef ISOSPEC_DEBUG
 #undef ISOSPEC_IMPOSSIBLE
 #include <cassert>
 #define ISOSPEC_IMPOSSIBLE(condition) assert(!(condition));
