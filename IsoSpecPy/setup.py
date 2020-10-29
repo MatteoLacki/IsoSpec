3b4c84cc6d73a53829f5026880db00295686df0d
Michał Startek <mist@mimuw.edu.pl>
2020-10-29 20:05:23 +0100

    Mark Python3.8 and 3.9 as supported

--- IsoSpecPy/setup.py
+++ IsoSpecPy/setup.py
@@ -94,6 +94,8 @@ setup_args = {
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     # What does your project relate to?
