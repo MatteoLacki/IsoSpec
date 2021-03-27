db66ae1969fcce0b04891a9dc06af3c220b815be
Michał Startek <mist@mimuw.edu.pl>
2021-03-27 14:18:09 +0100

    Another test

--- IsoSpec++/python-build.cpp
+++ IsoSpec++/python-build.cpp
@@ -6,15 +6,13 @@
 
 #if ISOSPEC_TEST_WE_ARE_ON_WINDOWS
 
-#error "kvjndfjkv"
-
 #include <Python.h>
 
 // Provide a dumy PyInit function on Windows/MSVC.
 // We're not using it, as we'll load using CFFI - but it's easier
 // than fighting with the build system.
 extern "C" {
-    PyObject* PyInit_modulename(void) { return nullptr; };
+    __declspec(dllexport) PyObject* PyInit_modulename(void) { return nullptr; };
 }
 
 #endif
