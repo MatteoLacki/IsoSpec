33c0033807488867d53f6bc91ae0fb2f85eb4cbd
Michał Startek <mist@mimuw.edu.pl>
2021-03-27 14:08:02 +0100

    PyInit needs to be extern "C"

--- IsoSpec++/python-build.cpp
+++ IsoSpec++/python-build.cpp
@@ -11,7 +11,9 @@
 // Provide a dumy PyInit function on Windows/MSVC.
 // We're not using it, as we'll load using CFFI - but it's easier
 // than fighting with the build system.
-PyObject* PyInit_modulename(void) { return nullptr; };
+extern "C" {
+    PyObject* PyInit_modulename(void) { return nullptr; };
+}
 
 #endif
 
