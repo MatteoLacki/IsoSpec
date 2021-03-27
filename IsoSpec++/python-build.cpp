9eaa6912018064e3cded8efa354c8eb524a166c3
Michał Startek <mist@mimuw.edu.pl>
2021-03-27 14:22:56 +0100

    Fix silly mistake

--- IsoSpec++/python-build.cpp
+++ IsoSpec++/python-build.cpp
@@ -12,7 +12,7 @@
 // We're not using it, as we'll load using CFFI - but it's easier
 // than fighting with the build system.
 extern "C" {
-    __declspec(dllexport) PyObject* PyInit_modulename(void) { return nullptr; };
+    __declspec(dllexport) PyObject* PyInit_IsoSpecCppPy(void) { return nullptr; };
 }
 
 #endif
