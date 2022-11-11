99e77d4ed89cfe7926108d9089ff8c24bff2c3c9
Michał Startek <michal.startek.uw@gmail.com>
2022-11-11 14:39:32 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup_args = {
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    'version': '2.2.0',
+    'version': '2.2.1',
 
     'description': 'Python interface to IsoSpec++ isotopic envelope calculator library',
     'long_description': 'Python interface to IsoSpec++ isotopic envelope calculator library',
