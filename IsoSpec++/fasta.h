90647e7ca8cc35367850870873dc2553948edd36
Mateusz Lacki <matteo.lacki@gmail.com>
2025-08-15 09:57:01 +0200

    Added docs and another function on C++ side.

--- IsoSpec++/fasta.h
+++ IsoSpec++/fasta.h
@@ -30,6 +30,11 @@ extern const double aa_elem_probabilities[19];
 
 extern const int aa_symbol_to_elem_counts[256*6];
 
+//! Count elemental composition of an unmodificed sequence of amino acids.
+/*!
+    WARNING!!! This function does deal with any questions regarding Water.
+    If you pass in a sequence (from a fasta, but someone here does not discern between the two), add water yourself. If you pass in a b or y fragment, add water yourself.
+*/
 inline void parse_fasta(const char* fasta, int atomCounts[6])
 {
     memset(atomCounts, 0, sizeof(decltype(atomCounts[0]))*6);
@@ -41,5 +46,22 @@ inline void parse_fasta(const char* fasta, int atomCounts[6])
             atomCounts[ii] += counts[ii];
     }
 }
+
+//! Count elemental composition of an unmodified (stripped and hot) sequeqnce.
+/*!
+    This function adds the missing water.
+*/
+inline void atom_counts_from_unmodified_sequence(const char* fasta, int atomCounts[6])
+{
+    parse_fasta(fasta, atomCounts)
+    // Add terminal water (H2O) for either precursor or fragment.
+    const int H_INDEX = 1; // Indexing: 0=C, 1=H, 2=N, 3=O, 4=S, 5=Se
+    const int O_INDEX = 3;
+
+    atomCounts[H_INDEX] += 2;
+    atomCounts[O_INDEX] += 1;
+}
+
+
 
 }  // namespace IsoSpec
