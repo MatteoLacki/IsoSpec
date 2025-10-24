995391cec7f3999712ff417da91706cc4b4d1239
Michał Startek <michal.startek@mimuw.edu.pl>
2025-10-24 14:41:30 +0200

    Fix for windows tests

--- .github/scripts/rmiso.py
+++ .github/scripts/rmiso.py
@@ -1,7 +1,8 @@
 import shutil
 from pathlib import Path
 
-path = Path(__file__).parent.parent / "IsoSpecPy"
+path = Path(__file__).parent.parent.parent / "IsoSpecPy"
 print(f"Removing IsoSpecPy directory at: {path}")
+print("Its contents are:", list(path.iterdir()))
 shutil.rmtree(path, ignore_errors=True)
 print("Done.")
